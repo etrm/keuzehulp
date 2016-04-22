97cddf1ccbf3711e97211ec4efdc924e3da22d1d
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2016-04-22 21:48:30 +0200

    move menu_structure to data folder

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ require 'yaml'
 class Menu
 
   def initialize
-    @menu = YAML.load_file(Rails.root.join('menu_structure.yml'))
+    @menu = YAML.load_file(Rails.root.join('data/menu_structure.yml'))
   end
 
   def hash
