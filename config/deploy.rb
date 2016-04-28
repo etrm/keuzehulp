05539c9e780957b871c464962da46d21019e1899
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2016-04-28 15:07:26 +0200

    Repair secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs,
